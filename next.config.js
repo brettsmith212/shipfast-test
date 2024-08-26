a4df0586e92a75497a152912b6905bd344ac5eb7
brettsmith212 <brettsmith212@gmail.com>
2024-08-25 20:55:43 -0700

    env

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
 			"logos-world.net",
 		],
 	},
+	env: {
+		NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+		NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+	},
 };
 
 module.exports = nextConfig;
