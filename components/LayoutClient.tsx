22f760b37297f085d1b165afe6348229a61bf978
brettsmith212 <brettsmith212@gmail.com>
2024-08-25 21:02:11 -0700

    another one

--- components/LayoutClient.tsx
+++ components/LayoutClient.tsx
@@ -2,7 +2,7 @@
 
 import { User } from "@supabase/supabase-js";
 import { createClient } from "@/libs/supabase/client";
-import { useEffect, useState, ReactNode } from "react";
+import { useEffect, useState, ReactNode, useMemo } from "react";
 import { usePathname } from "next/navigation";
 import { Crisp } from "crisp-sdk-web";
 import NextTopLoader from "nextjs-toploader";
@@ -10,55 +10,57 @@ import { Toaster } from "react-hot-toast";
 import { Tooltip } from "react-tooltip";
 import config from "@/config";
 
+const supabase = createClient();
 // Crisp customer chat support:
 // This component is separated from ClientLayout because it needs to be wrapped with <SessionProvider> to use useSession() hook
 const CrispChat = (): null => {
-  const pathname = usePathname();
+	const pathname = usePathname();
 
-  const supabase = createClient();
-  const [data, setData] = useState<{ user: User }>(null);
+	const [data, setData] = useState<{ user: User }>(null);
 
-  // This is used to get the user data from Supabase Auth (if logged in) => user ID is used to identify users in Crisp
-  useEffect(() => {
-    const getUser = async () => {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
+	// This is used to get the user data from Supabase Auth (if logged in) => user ID is used to identify users in Crisp
 
-      if (user) {
-        setData({ user });
-      }
-    };
-    getUser();
-  }, []);
+	const auth = useMemo(() => supabase.auth, []);
+	useEffect(() => {
+		const getUser = async () => {
+			const {
+				data: { user },
+			} = await auth.getUser();
 
-  useEffect(() => {
-    if (config?.crisp?.id) {
-      // Set up Crisp
-      Crisp.configure(config.crisp.id);
+			if (user) {
+				setData({ user });
+			}
+		};
+		getUser();
+	}, [auth]);
 
-      // (Optional) If onlyShowOnRoutes array is not empty in config.js file, Crisp will be hidden on the routes in the array.
-      // Use <AppButtonSupport> instead to show it (user clicks on the button to show Crisp—it cleans the UI)
-      if (
-        config.crisp.onlyShowOnRoutes &&
-        !config.crisp.onlyShowOnRoutes?.includes(pathname)
-      ) {
-        Crisp.chat.hide();
-        Crisp.chat.onChatClosed(() => {
-          Crisp.chat.hide();
-        });
-      }
-    }
-  }, [pathname]);
+	useEffect(() => {
+		if (config?.crisp?.id) {
+			// Set up Crisp
+			Crisp.configure(config.crisp.id);
 
-  // Add User Unique ID to Crisp to easily identify users when reaching support (optional)
-  useEffect(() => {
-    if (data?.user && config?.crisp?.id) {
-      Crisp.session.setData({ userId: data.user?.id });
-    }
-  }, [data]);
+			// (Optional) If onlyShowOnRoutes array is not empty in config.js file, Crisp will be hidden on the routes in the array.
+			// Use <AppButtonSupport> instead to show it (user clicks on the button to show Crisp—it cleans the UI)
+			if (
+				config.crisp.onlyShowOnRoutes &&
+				!config.crisp.onlyShowOnRoutes?.includes(pathname)
+			) {
+				Crisp.chat.hide();
+				Crisp.chat.onChatClosed(() => {
+					Crisp.chat.hide();
+				});
+			}
+		}
+	}, [pathname]);
 
-  return null;
+	// Add User Unique ID to Crisp to easily identify users when reaching support (optional)
+	useEffect(() => {
+		if (data?.user && config?.crisp?.id) {
+			Crisp.session.setData({ userId: data.user?.id });
+		}
+	}, [data]);
+
+	return null;
 };
 
 // All the client wrappers are here (they can't be in server components)
@@ -67,31 +69,31 @@ const CrispChat = (): null => {
 // 3. Tooltip: Show tooltips if any JSX elements has these 2 attributes: data-tooltip-id="tooltip" data-tooltip-content=""
 // 4. CrispChat: Set Crisp customer chat support (see above)
 const ClientLayout = ({ children }: { children: ReactNode }) => {
-  return (
-    <>
-      {/* Show a progress bar at the top when navigating between pages */}
-      <NextTopLoader color={config.colors.main} showSpinner={false} />
+	return (
+		<>
+			{/* Show a progress bar at the top when navigating between pages */}
+			<NextTopLoader color={config.colors.main} showSpinner={false} />
 
-      {/* Content inside app/page.js files  */}
-      {children}
+			{/* Content inside app/page.js files  */}
+			{children}
 
-      {/* Show Success/Error messages anywhere from the app with toast() */}
-      <Toaster
-        toastOptions={{
-          duration: 3000,
-        }}
-      />
+			{/* Show Success/Error messages anywhere from the app with toast() */}
+			<Toaster
+				toastOptions={{
+					duration: 3000,
+				}}
+			/>
 
-      {/* Show tooltips if any JSX elements has these 2 attributes: data-tooltip-id="tooltip" data-tooltip-content="" */}
-      <Tooltip
-        id="tooltip"
-        className="z-[60] !opacity-100 max-w-sm shadow-lg"
-      />
+			{/* Show tooltips if any JSX elements has these 2 attributes: data-tooltip-id="tooltip" data-tooltip-content="" */}
+			<Tooltip
+				id="tooltip"
+				className="z-[60] !opacity-100 max-w-sm shadow-lg"
+			/>
 
-      {/* Set Crisp customer chat support */}
-      <CrispChat />
-    </>
-  );
+			{/* Set Crisp customer chat support */}
+			<CrispChat />
+		</>
+	);
 };
 
 export default ClientLayout;
--- next.config.js
+++ next.config.js
@@ -11,10 +11,6 @@ const nextConfig = {
 			"logos-world.net",
 		],
 	},
-	env: {
-		NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-		NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-	},
 };
 
 module.exports = nextConfig;
