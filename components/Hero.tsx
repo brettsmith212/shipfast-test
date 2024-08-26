26049cb3547c460eabf580aa2186511e8c059a23
brettsmith212 <brettsmith212@gmail.com>
2024-08-25 19:31:36 -0700

    docker test 1

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,13 +7,15 @@ const Hero = () => {
 		<section className="max-w-7xl mx-auto bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
 			<div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-center lg:text-left lg:items-start">
 				<h1 className="font-extrabold text-4xl lg:text-6xl tracking-tight md:-mb-4">
-					AI + Forums = Your Next Big Idea
+					Docker Test 1
 				</h1>
 				<p className="text-lg opacity-80 leading-relaxed">
-					The NextJS boilerplate with all you need to build your SaaS, AI tool, or any other web
-					app. From idea to production in 5 minutes.
+					The NextJS boilerplate with all you need to build your SaaS, AI tool,
+					or any other web app. From idea to production in 5 minutes.
 				</p>
-				<button className="btn btn-primary btn-wide">Get {config.appName}</button>
+				<button className="btn btn-primary btn-wide">
+					Get {config.appName}
+				</button>
 
 				<TestimonialsAvatars priority={true} />
 			</div>
